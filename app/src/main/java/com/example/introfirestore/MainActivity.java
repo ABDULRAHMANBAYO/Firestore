5504294c203bc7b40264d629f7fee976200afcbd
SULTAN <ramhziworld@gmail.com>
2020-05-11 21:37:38 +0100

    Updated CRUD function

--- app/src/main/java/com/example/introfirestore/MainActivity.java
+++ app/src/main/java/com/example/introfirestore/MainActivity.java
@@ -15,12 +15,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText enterThought;
     private Button saveButton;
     private TextView titleTextview;
-    private TextView thoughtTextview;
+    //    private TextView thoughtTextview;
     private Button showDetails;
     private Button updateButton;
     private Button deleteButton;
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference documentReference = db.collection("Journal")
             .document("First thought");
+    private CollectionReference collectionReference = db.collection("Journal");
 
 
     @Override
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         enterText = findViewById(R.id.editText);
         enterThought = findViewById(R.id.editThought);
         saveButton = findViewById(R.id.saveButton);
-        thoughtTextview = findViewById(R.id.rec_thought);
+//        thoughtTextview = findViewById(R.id.rec_thought);
         titleTextview = findViewById(R.id.rec_title);
         showDetails = findViewById(R.id.showDetailButton);
         updateButton = findViewById(R.id.updateButton);
@@ -66,14 +69,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         deleteButton.setOnClickListener(this);
     }
 
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.saveButton:
-                saveDetails();
+                addThought();
                 break;
             case R.id.showDetailButton:
-                showDetails();
+                getThought();
                 break;
             case R.id.updateButton:
                 update();
@@ -84,57 +88,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void showDetails() {
-        documentReference
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            Journal journal = documentSnapshot.toObject(Journal.class);
-                            if (journal != null) {
-                                titleTextview.setText(journal.getTitle());
-                                thoughtTextview.setText(journal.getThought());
-                            }
-
-
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Failed ", Toast.LENGTH_LONG)
-                                    .show();
-                        }
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), "Failed " + e.toString(), Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
-    }
-
-    private void saveDetails() {
+    private void addThought() {
         String title = enterText.getText().toString().trim();
         String thought = enterThought.getText().toString().trim();
 
-        Journal journal = new Journal();
-        journal.setTitle(title);
-journal.setThought(thought);
-//        Map<String, Object> data = new HashMap<>();
-//
-//        data.put(KEY_TITLE, title);
-//        data.put(KEY_THOUGHT, thought);
+        Journal journal = new Journal(title,thought);
 
-        documentReference
-                .set(journal)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+        collectionReference.add(journal)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
+                    public void onSuccess(DocumentReference documentReference) {
                         Toast.makeText(getApplicationContext(), "Saved successfully", Toast.LENGTH_SHORT)
                                 .show();
                     }
                 })
+
 
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -143,6 +111,70 @@ journal.setThought(thought);
                                 .show();
                     }
                 });
+    }
+
+    private void showDetails() {
+        getThought();
+//        documentReference
+//                .get()
+//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        if (documentSnapshot.exists()) {
+//                            Journal journal = documentSnapshot.toObject(Journal.class);
+//                            if (journal != null) {
+//                                titleTextview.setText(journal.getTitle());
+//                                thoughtTextview.setText(journal.getThought());
+//                            }
+//
+//
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Failed ", Toast.LENGTH_LONG)
+//                                    .show();
+//                        }
+//
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext(), "Failed " + e.toString(), Toast.LENGTH_LONG)
+//                                .show();
+//                    }
+//                });
+    }
+
+    private void saveDetails() {
+
+        addThought();
+//        String title = enterText.getText().toString().trim();
+//        String thought = enterThought.getText().toString().trim();
+//
+//        Journal journal = new Journal();
+//        journal.setTitle(title);
+//        journal.setThought(thought);
+//        Map<String, Object> data = new HashMap<>();
+//
+//        data.put(KEY_TITLE, title);
+//        data.put(KEY_THOUGHT, thought);
+
+//        documentReference
+//                .set(journal)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Toast.makeText(getApplicationContext(), "Saved successfully", Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                })
+//
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext(), "Saved Failed " + e.toString(), Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                });
     }
 
     private void update() {
@@ -154,7 +186,8 @@ journal.setThought(thought);
         data.put(KEY_TITLE, title);
 //        data.put(KEY_THOUGHT, thought);
 
-        documentReference.update(data).addOnSuccessListener(
+        collectionReference.
+                 document().update(data).addOnSuccessListener(
                 new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -170,11 +203,48 @@ journal.setThought(thought);
                         .show();
             }
         });
+
+//        documentReference.update(data).addOnSuccessListener(
+//                new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Toast.makeText(getApplicationContext(), "Updated successfully", Toast.LENGTH_SHORT)
+//                                .show();
+//
+//                    }
+//                }
+//        ).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getApplicationContext(), "Saved Failed " + e.toString(), Toast.LENGTH_SHORT)
+//                        .show();
+//            }
+//        });
     }
 
     private void delete() {
-        //delete all
-        documentReference.delete();
+
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        String data = "";
+//                        StringBuilder stringBuilder = new StringBuilder();
+                        for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+
+                            Journal journal = snapshots.toObject(Journal.class);
+                            data += "Title :" + journal.getTitle() + "\n" +
+                                    "Journal :" + journal.getThought() + "\n\n";
+
+
+                        }
+                        //delete all
+                        collectionReference.document().update(data,FieldValue.delete());
+
+
+                    }
+                });
 
         //DELETE A FIELD
 //        documentReference.update(KEY_THOUGHT, FieldValue.delete()).addOnSuccessListener(
@@ -195,34 +265,95 @@ journal.setThought(thought);
 //        });
     }
 
+    private void getThought() {
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        String data = "";
+//                        StringBuilder stringBuilder = new StringBuilder();
+                        for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+
+                            Journal journal = snapshots.toObject(Journal.class);
+                            data += "Title :" + journal.getTitle() + "\n" +
+                                    "Journal :" + journal.getThought() + "\n\n";
+
+
+                        }
+                        titleTextview.setText(data);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Saved Failed " + e.toString(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+
+        collectionReference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
-                                @Nullable FirebaseFirestoreException e) {
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
                     Toast.makeText(getApplicationContext(), "Failed " + e.toString(), Toast.LENGTH_SHORT)
                             .show();
 
                 }
-                if (documentSnapshot != null && documentSnapshot.exists()) {
-                    Journal journal = documentSnapshot.toObject(Journal.class);
-                    if (journal != null) {
-                        titleTextview.setText(journal.getTitle());
-                        thoughtTextview.setText(journal.getThought());
+                String data = "";
+                if (queryDocumentSnapshots != null) {
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+
+                        Journal journal = snapshots.toObject(Journal.class);
+                        data += "Title :" + journal.getTitle() + "\n" +
+                                "Journal :" + journal.getThought() + "\n\n";
                     }
-
-                } else {
-                    titleTextview.setText("");
-                    thoughtTextview.setText("");
-
                 }
-
+                titleTextview.setText(data);
             }
         });
     }
-
 }
+
+//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+//                                @Nullable FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    Toast.makeText(getApplicationContext(), "Failed " + e.toString(), Toast.LENGTH_SHORT)
+//                            .show();
+//
+//                } String data="";
+//                if (documentSnapshot != null && documentSnapshot.exists()) {
+//                    Journal journal = documentSnapshot.toObject(Journal.class);
+//                    if (journal != null) {
+//
+//                        data = "Title :" + journal.getTitle() + "\n" +
+//                                "Journal :"  + journal.getThought();
+//                        titleTextview.setText(data);
+////                        titleTextview.setText(journal.getTitle());
+////                        thoughtTextview.setText(journal.getThought());
+//                    }
+//
+//                } else {
+//
+//                    titleTextview.setText(data);
+////                    titleTextview.setText("");
+////                    thoughtTextview.setText("");
+//
+//                }
+//
+//            }
+//        });
+
+
+
 
